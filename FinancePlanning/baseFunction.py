fc8eae002e5828e8c1d6252f14cf0b19b3442853
ssivak1997 <77776240+ssivak1997@users.noreply.github.com>
2023-12-05 15:03:18 +0530

    Added some more selection statements to optimize the alternate possibilities

--- FinancePlanning/baseFunction.py
+++ FinancePlanning/baseFunction.py
@@ -5,14 +5,15 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 father_name = input("Enter your father name: ")
 x = input("Is " + father_name + " alive? (Y/N): ")
-if x == 'Y' or x == 'y':
+if x in ("Y", "y"):
     father_age = int(input("Enter " + father_name + "'s age: "))
 mother_name = input("Enter your mother name: ")
 y = input("Is " + mother_name + " alive? (Y/N): ")
-if y == 'Y' or y == 'y':
+if y in ("Y", "y"):
     mother_age = int(input("Enter " + mother_name + "'s age: "))
 print("Select your marital status:\n1. Single\n2. Married")
 marital_status = input("Marital Status: ")
+child_count = 0
 child = []
 child_age = []
 child_education_planned_age = []
@@ -35,40 +36,42 @@ if marital_status in ('Married', 'married', 'M', 'm', '2'):
 income = int(input("Enter your total monthly income: "))
 expense = int(input("Enter your monthly expenses: "))
 tli = input("Availed Term Life Insurance? (Y/N): ")
-if tli == 'Y' or tli == 'y':
+if tli in ("Y", "y"):
     tli_sum = input("Enter the Term Life Insurance Sum Amount: ")
 else:
     tli_sum = 0
 mediclaim = input("Availed Medi-Claim? (Y/N): ")
-if mediclaim == 'Y' or mediclaim == 'y':
-    mediclaim_sum = input("Enter the Medi-Claim Sum Amount: ")
+if mediclaim in ("Y", "y"):
+    mediclaim_sum = int(input("Enter the Medi-Claim Sum Amount: "))
+    mediclaim_premium = int(input("Enter the Medi_Claim Premium Amount per month:: "))
 else:
     mediclaim_sum = 0
+    mediclaim_premium = 0
 hl = input("Have you availed housing loan? (Y/N):")
-if hl == 'Y' or hl == 'y':
+if hl in ("Y", "y"):
     housing_loan = int(input("Enter the value of housing loan amount: "))
     housing_loan_emi = int(input("Enter the monthly emi amount: "))
 else:
     housing_loan = 0
     housing_loan_emi = 0
     house_goal = input("Are you planning to have a new house? (Y/N) ")
-    if house_goal == 'Y' or house_goal == 'y':
+    if house_goal in ("Y", "y"):
         house_estmtn = int(input("Enter the estimated budget of the house you are planning to have: "))
         house_eta = int(input("Enter the number of years from now when you are planning to begin the house work: "))
         house_savings = int(input("Enter the current amount of savings for the new house: "))
 cl = input("Have you availed car loan? (Y/N):")
-if cl == 'Y' or cl == 'y':
+if cl in ("Y", "y"):
     car_loan = int(input("Enter the value of car loan amount: "))
     car_loan_emi = int(input("Enter the monthly emi amount: "))
 else:
     car_loan = 0
     car_loan_emi = 0
     car_goal = input("Are you planning to buy a new car? (Y/N) ")
-    if car_goal == 'Y' or car_goal == 'y':
+    if car_goal in ("Y", "y"):
         car_estmtn = int(input("Enter the estimated budget of the car you are planning to buy: "))
         car_eta = int(input("Enter the number of years from now when you are planning to buy the car: "))
         car_savings = int(input("Enter the current amount of savings for the new car: "))
-if marital_status in ('Married', 'married', 'M', 'm', '2'):
+if marital_status in ('Married', 'married', 'M', 'm', '2') and child_count > 0:
     i = 1
     while i <= int(child_count):
         e_age = int(input("Enter the age of " + child[i - 1] + "'s education plan to begin with: "))
@@ -90,11 +93,11 @@ gold_loan = int(input("Enter the value of gold loan amount: "))
 misc_loan = int(input("Enter the value of miscellaneous loan amount: "))
 print("The value of housing loan is: " + str(housing_loan))
 print("The value of car loan is: " + str(car_loan))
-if hl == 'Y' or hl == 'y' and cl == 'Y' or cl == 'y':
+if hl in("Y", "y") and cl in ("Y", "y"):
     emi = housing_loan_emi + car_loan_emi
-elif hl == 'Y' or hl == 'y' and cl == 'N' or cl == 'n':
+elif hl in("Y", "y") and cl in ("N", "n"):
     emi = housing_loan_emi
-elif hl == 'N' or hl == 'n' and cl == 'Y' or cl == 'y':
+elif hl in("N", "n") and cl in ("Y", "y"):
     emi = car_loan_emi
 else:
     emi = 0
@@ -113,14 +116,14 @@ ideal_medi_claim_sum = calc_ideal_medic_insurance(income)
 ideal_medi_claim_premium = calc_ideal_medic_insurance_premium(income)
 print("\nPROTECTIVE INVESTMENT:")
 print(" \t\t\t\t\t\t\t\tCURRENT VALUE\tIDEAL VALUE")
-print("Term Life Insurance \t\t\t\t" + str(tli_sum) + " \t" + str(ideal_tli))
-print("Medical Insurance - Sum Insured \t" + str(mediclaim_sum) + " \t\t" + str(ideal_medi_claim_sum))
-print("Medical Insurance - Premium \t\t" + "0" + " \t\t\t\t" + str(ideal_medi_claim_premium))
+print("Term Life Insurance \t\t\t\t" + str(tli_sum) + " \t\t\t\t\t\t\t\t\t" + str(ideal_tli))
+print("Medical Insurance - Sum Insured \t" + str(mediclaim_sum) + " \t\t\t\t\t\t" + str(ideal_medi_claim_sum))
+print("Medical Insurance - Premium \t\t" + str(mediclaim_premium) + " \t\t\t\t" + str(ideal_medi_claim_premium))
 print("*" * 10)
 
 # Calculating the Short Term Goals and its optimal planning
 print("\nSHORT TERM GOALS:")
-if hl == 'N' or hl == 'n':
+if hl in ("N","n") and house_goal in ("Y","y"):
     ideal_housing_goal = calc_housing_plan(house_estmtn, house_eta, house_savings, exp_growth_rate)
     print("1. House Goal:")
     print("Total value of house: " + str(house_estmtn))
@@ -128,7 +131,7 @@ if hl == 'N' or hl == 'n':
     print("Current Savings for downpayment: " + str(house_savings))
     print("Balance to be saved: " + str(int(int(house_estmtn) * 0.3) - int(house_savings)))
     print("Monthly savings required for down payment: " + str(ideal_housing_goal))
-elif cl == 'N' or cl == 'n':
+elif cl in ("N","n") and car_goal in ("Y","y"):
     ideal_car_goal = calc_car_plan(car_estmtn, car_eta, car_savings, exp_growth_rate)
     print("\n2. Car Goal:")
     print("Total value of car: " + str(car_estmtn))
@@ -144,21 +147,26 @@ print("*" * 10)
 print("\nLONG TERM GOALS:")
 infl_adj_edcn_cost = []
 infl_adj_mrrg_cost = []
-for i in range(child_count):
-    edctn_cost = calc_edcn_plan(child_age[i], child_education_planned_age[i], child_education_cost[i], child_education_current_savings[i], exp_growth_rate, exp_inflation_rate)
-    infl_adj_edcn_cost.append(edctn_cost)
-    print("Education Plan for " + child[i]+ ":")
-    print("Current age of "+ child[i]+": " + str(child_age[i]))
-    print("Age of " + child[i] + " while entering higher education: "+str(child_education_planned_age[i]))
-    print("Current cost of  higher education of " + child[i] + ": "+ str(child_education_cost[i]))
-    print("Monthly savings required for higher education of " + child[i] + ": " + str(infl_adj_edcn_cost[i]))
-    mrrg_cost = calc_mrrg_plan(child_age[i], child_marriage_planned_age[i], child_marriage_cost[i], child_marriage_current_savings[i], exp_growth_rate, exp_inflation_rate)
-    infl_adj_mrrg_cost.append(mrrg_cost)
-    print("\nMarriage Plan for " + child[i] + ":")
-    print("Current age of " + child[i] + ": " + str(child_age[i]))
-    print("Planned marriage age of " + child[i] + ": " + str(child_marriage_planned_age[i]))
-    print("Current cost of  marriage of " + child[i] + ": " + str(child_marriage_cost[i]))
-    print("Monthly savings required for marriage of " + child[i] + ": " + str(infl_adj_mrrg_cost[i]))
+if child_count > 0:
+    for i in range(child_count):
+        edctn_cost = calc_edcn_plan(child_age[i], child_education_planned_age[i], child_education_cost[i],
+                                    child_education_current_savings[i], exp_growth_rate, exp_inflation_rate)
+        infl_adj_edcn_cost.append(edctn_cost)
+        print("Education Plan for " + child[i] + ":")
+        print("Current age of " + child[i] + ": " + str(child_age[i]))
+        print("Age of " + child[i] + " while entering higher education: " + str(child_education_planned_age[i]))
+        print("Current cost of  higher education of " + child[i] + ": " + str(child_education_cost[i]))
+        print("Monthly savings required for higher education of " + child[i] + ": " + str(infl_adj_edcn_cost[i]))
+        mrrg_cost = calc_mrrg_plan(child_age[i], child_marriage_planned_age[i], child_marriage_cost[i],
+                                   child_marriage_current_savings[i], exp_growth_rate, exp_inflation_rate)
+        infl_adj_mrrg_cost.append(mrrg_cost)
+        print("\nMarriage Plan for " + child[i] + ":")
+        print("Current age of " + child[i] + ": " + str(child_age[i]))
+        print("Planned marriage age of " + child[i] + ": " + str(child_marriage_planned_age[i]))
+        print("Current cost of  marriage of " + child[i] + ": " + str(child_marriage_cost[i]))
+        print("Monthly savings required for marriage of " + child[i] + ": " + str(infl_adj_mrrg_cost[i]))
+else:
+    print("TBP")
 print("*"*10)
 
 # Calculating the retirement plan
@@ -182,6 +190,9 @@ if x in ("Y", "y") and y in ("Y", "y"):
 elif x in ("Y", "y") or y in ("Y", "y"):
     parental_monthly_tli = monthly_tli*0.5
     parental_monthly_mediclaim = monthly_mediclaim*0.5
+else:
+    parental_monthly_tli = 0
+    parental_monthly_mediclaim = 0
 print("Total Income: " + str(income))
 print("Total Expenses: " + str(expense))
 print("\nMonthly contribution to Term Insurance: " + str(monthly_tli))
@@ -194,13 +205,16 @@ print("\nHousing Loan EMI: " + str(housing_loan_emi))
 print("Car Loan EMI: " + str(car_loan_emi))
 print("Total EMI: " + str(emi))
 print("\n")
-for i in range(child_count):
-    print("Savings for Education of " + child[i] +" : " + str(infl_adj_edcn_cost[i]))
-    print("Savigns for Marriage of " + child[i] +" : " + str(infl_adj_mrrg_cost[i]))
-print("Savings for Retirement: " + str(int(pst_rtrmnt_savings)))
-edctn_savings = sum(infl_adj_edcn_cost)
-marriage_savigns = sum(infl_adj_mrrg_cost)
-total_savings = int(pst_rtrmnt_savings+edctn_savings+marriage_savigns)
+if child_count > 0:
+    for i in range(child_count):
+        print("Savings for Education of " + child[i] + " : " + str(infl_adj_edcn_cost[i]))
+        print("Savigns for Marriage of " + child[i] + " : " + str(infl_adj_mrrg_cost[i]))
+    print("Savings for Retirement: " + str(int(pst_rtrmnt_savings)))
+    edctn_savings = sum(infl_adj_edcn_cost)
+    marriage_savigns = sum(infl_adj_mrrg_cost)
+    total_savings = int(pst_rtrmnt_savings + edctn_savings + marriage_savigns)
+else:
+    total_savings = int(pst_rtrmnt_savings)
 print("Total Savings: " + str(total_savings))
 net_amnt = income - (total_savings+emi+total_insurance+expense)
 print("\nNet amount of income-expense summary is " + str(net_amnt))
